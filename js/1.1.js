eb6dd87835653009894598ff1d153d5cb8b41327
SerhiiKot07 <sergey.kotelnykov@gmail.com>
2023-01-13 00:38:20 +0200

    Update 1.1.js

--- js/1.1.js
+++ js/1.1.js
@@ -25,16 +25,33 @@ console.log(type);
 // ---------------------------------------
 // - Ввод користувача
 // - Методи window.confirm() i window.prompt()
-const shouldRenew = confirm("Бажаєте подовжити підписку?");
-console.log(shouldRenew);
+// const shouldRenew = confirm("Бажаєте подовжити підписку?");
+// console.log(shouldRenew);
 
-const quantityA = prompt("Введіть кількість товарів");
-console.log(quantityA);
-console.log(typeof quantityA);
+// const quantityA = prompt("Введіть кількість товарів");
+// console.log(quantityA);
+// console.log(typeof quantityA);
 
-let quantityB = prompt("Введіть кількість товарів");
-quantityB = Number(quantityB);
-console.log(quantityB);
-console.log(typeof quantityB);
+// let quantityB = prompt("Введіть кількість товарів");
+// quantityB = Number(quantityB);
+// console.log(quantityB);
+// console.log(typeof quantityB);
 
-// ------------------------------------
+// --------------------- parseInt()
+let elementWidth = "50px";
+elementWidth = parseInt(elementWidth);
+console.log(elementWidth); // 50
+console.log(typeof elementWidth); // number
+
+// ------------------ parseFloat()
+let elementHeigth = "123.123456px";
+elementHeigth = parseFloat(elementHeigth);
+console.log(elementHeigth); // 123.123456
+console.log(typeof elementHeigth); // number
+
+// ----------------- .toFixed() - кільсть символів після крапки, округлює
+let salary = 130.164756789;
+salary = Number(salary.toFixed(3));
+console.log(salary);
+
+// -----------------------
